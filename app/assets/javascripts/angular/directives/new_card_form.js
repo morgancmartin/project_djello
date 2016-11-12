8bcc8d0f4640ed36ee9bd481f01273c1ed6a5bf3
Morgan Martin <morganm5201@gmail.com>
2016-11-12 17:45:41 -0500

    implement vertical scroll for lists

--- app/assets/javascripts/angular/directives/new_card_form.js
+++ app/assets/javascripts/angular/directives/new_card_form.js
@@ -6,24 +6,38 @@ DjelloApp.directive('newCardForm', ['$timeout', function($timeout) {
       list: '='
     },
     link: function($scope, element, attrs){
+
       $scope.createNewCard = function(list, title){
         var params = { title: title };
         list.createCard(params);
         $scope.newCardForm = {};
         $scope.showNewCardForm = false;
       };
+
       $scope.focusOut = function(){
         $timeout(function(){
           $scope.showNewCardForm = false;
         }, 125);
       };
+
       $scope.$watch('showNewCardForm', function(value){
         if(value){
           $timeout(function() {
             element.find('#newCardInput')[0].focus();
+            $scope.attachEnterListener();
           });
         }
       });
+
+      $scope.attachEnterListener = function(){
+        element.find("#newCardInput").keypress(function(event) {
+          if (event.which == 13) {
+            event.preventDefault();
+            $scope.createNewCard($scope.list, element.find('#newCardInput').val());
+          }
+        });
+      };
+
     }
   };
 }]);
