07a1a6a663fd63764b9e2ea79b29aa84f7281e87
Morgan Martin <morganm5201@gmail.com>
2016-11-14 11:06:46 -0500

    convert nav and boards button to thimble css

--- app/assets/javascripts/angular/controllers/navbar_controller.js
+++ app/assets/javascripts/angular/controllers/navbar_controller.js
@@ -3,6 +3,7 @@ DjelloApp.controller("navCtrl", ['$scope', 'Restangular', 'Auth', 'boards', 'boa
   $scope.boards = boards;
   $scope.showBoardsDropdown = false;
   $scope.toggleBoardsDropdown = function($event) {
+    console.log('toggling');
     if(!$scope.justToggledBoards){
       $scope.showBoardsDropdown = !$scope.showBoardsDropdown;
       if($scope.showBoardsDropdown){
@@ -24,14 +25,8 @@ DjelloApp.controller("navCtrl", ['$scope', 'Restangular', 'Auth', 'boards', 'boa
     $scope.djelloImg = angular.element('#djello-img').remove();
     angular.element('#my-brand').prepend($scope.djelloImg);
     angular.element('#my-brand').prepend($scope.jelloImg);
-    angular.element('#boards-button').prepend($scope.jelloImg.clone());
     $scope.logoutLink = angular.element('#navbar').data('logout-link');
   })();
-
-  $scope.goToBoard = function(boardId){
-    $state.go('home.show', {id: boardId});
-    $scope.toggleBoardsDropdown();
-  };
 
   Auth.currentUser()
     .then(function(user) {
